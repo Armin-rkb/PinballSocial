fdeb1cf72175816e89f51bb7c91e77f8926142af
Armin Karimi Birgani <armin.rkb@gmail.com>
2016-10-30 12:39:38 +0100

    First commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Facebook.Unity;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject notLoggedInUI;
+    [SerializeField]
+    private GameObject LoggedInUI;
+
+	void Awake ()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(InitCallback);
+        }
+	}
+
+    void InitCallback()
+    {
+        print("Facebook succesfully initialized!");
+    }
+
+    // Login function for our button.
+    public void Login()
+    {
+        if (!FB.IsLoggedIn)
+        {
+            FB.LogInWithReadPermissions(new List<string> {"user_friends"}, LoginCallback);
+        }
+    }
+
+    // Check if the login went succesfull.
+    void LoginCallback(ILoginResult result)
+    {
+        if(result.Error == null)
+        {
+            print("Login went succesfull");
+        }
+        else
+        {
+            Debug.Log("Login error: " + result.Error);
+        }
+    }
+}
